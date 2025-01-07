92d6bc89797a5915f217a55a64a232c28c7744b6
Myles Stubbs <jrstubbies@gmail.com>
2025-01-07 17:55:58 +1300

    added comments to html, css, and JS files

--- sitejs.js
+++ sitejs.js
@@ -1,16 +1,16 @@
+// initialise to start slideshow from beginning
 let slideIndex = 0;
-showSlides(slideIndex);
 
+// function called by the next/prev arrow buttons to change the slide
 function changeSlides(n) {
     showSlides(slideIndex += n)
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n)
-}
-
+// function to move slides along
 function showSlides(n) {
     let i;
+    
+    // get a list of all the slide containers using the class name 
     let slides = document.getElementsByClassName("slideshow-pane");
     
     // if move too far right then loop back to start
@@ -18,14 +18,16 @@ function showSlides(n) {
         slideIndex = 0;
     }
 
-    // if move to the left too far then loop back to the last slide 
+    // if move too far left then loop back to the last slide 
     if(n < 0) {
         slideIndex = slides.length;
     }
 
+    // hide all slides
     for(i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    // then display the current slide
     slides[slideIndex].style.display = "block";
 }
